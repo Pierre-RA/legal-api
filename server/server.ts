654c8ad6bbe43610cc756303dc2c8dbd0484fbf6
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2017-07-29 12:06:57 +0200

    chore: add CORS to server

--- server/server.ts
+++ server/server.ts
@@ -35,6 +35,12 @@ app.use(passport.initialize());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get(
   '/',
   homeController.index
