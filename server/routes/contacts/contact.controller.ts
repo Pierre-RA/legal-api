e368b9e8aa05dedd8901f8003a03bf8f7a74db1b
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2017-08-02 10:16:21 +0200

    feat: add count for type

--- server/routes/contacts/contact.controller.ts
+++ server/routes/contacts/contact.controller.ts
@@ -31,6 +31,17 @@ router.get('/count', (req: express.Request, res: express.Response) => {
   });
 });
 
+router.get('/count/:type', (req: express.Request, res: express.Response) => {
+  Contact.find({ type: req.params.type }).count((err, count) => {
+    if (err) {
+      return devError(err, res);
+    }
+    return res.json({
+      count: count
+    });
+  });
+});
+
 router.get('/:id', (req: express.Request, res: express.Response) => {
   Contact.findOne({ id: req.params.id }, (err, doc) => {
     if (err) {
