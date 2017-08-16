e55fbe534acd06a376dffb9b5a18204b3431ca99
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2017-08-16 17:53:26 +0200

    chore: improve coverage for userController

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -24,3 +24,75 @@ describe('GET /users', () => {
       });
   });
 });
+
+describe('login process', () => {
+  it('POST /login should return 401 with no data', () => {
+    return request(app)
+      .post('/login')
+      .expect(401)
+      .then(res => {
+        expect(res.body).have.property('message');
+      });
+  });
+  it('POST /login should return 401 with wrong user', () => {
+    return request(app)
+      .post('/login')
+      .send({
+        email: 'blob@blob.com',
+        password: 'blob',
+      })
+      .expect(401)
+      .then(res => {
+        expect(res.body).have.property('message');
+      });
+  });
+  it('POST /signup complex process', () => {
+    let user = {
+      email: 'test@test.com',
+      password: 'simple-test-password',
+    };
+    let id;
+    let token;
+    return request(app)
+      .post('/signup')
+      .send(user)
+      .expect(200)
+      .then(res => {
+        expect(res.body).have.property('_id');
+        id = res.body._id;
+        return request(app)
+          .post('/login')
+          .send({
+            email: user.email,
+            password: 'wrong-password'
+          })
+          .expect(401)
+      })
+      .then(res => {
+        return request(app)
+          .post('/login')
+          .send({
+            email: user.email,
+            password: user.password
+          })
+          .expect(200)
+      })
+      .then(res => {
+        expect(res.body).have.property('token');
+        token = res.body.token;
+        return request(app)
+          .get('/users/')
+          .expect(200)
+      })
+      .then(res => {
+        return request(app)
+          .get('/users/count')
+          .expect(200)
+      })
+      .then(res => {
+        return request(app)
+          .delete('/users/' + id)
+          .expect(200);
+      });
+  });
+});
